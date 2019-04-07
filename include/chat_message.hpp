fb0123a76ed8b48a082ec3efe6f860b015c5038e
huntern54 <hunter.nghiem0@gmail.com>
2019-04-07 17:24:30 -0500

    added sender and receiver

--- include/chat_message.hpp
+++ include/chat_message.hpp
@@ -1,98 +1,116 @@
+//
+// chat_message.hpp
+// ~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef CHAT_MESSAGE_HPP
 #define CHAT_MESSAGE_HPP
 
-#include "common_header.hpp"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 class chat_message
 {
-  public:
-    enum { header_length = 5 };
-    enum { max_body_length = 512 };
+public:
+  enum { header_length = 4 };
+  enum { max_body_length = 512 };
 
-    //default constructor
-    chat_message() : body_length_(0) {}
+  //sender
+  //boost uuids makes a uuid in order to send out 
+  boost::uuids::random_generator gen; 
+  boost::uuids::uuid send_out;
 
-    const char* data() const
-    {
-      return data_;
-    }
+  //recieve
+  boost::uuids::uuid take_in;
 
-    char* data()
-    {
-      return data_;
-    }
+  //string to lexical casting
+  std::string sender,receiver;
 
-    std::size_t length() const
-    {
-      return header_length + body_length_;
-    }
-
-    const char* body() const
-    {
-      return data_ + header_length;
-    }
-
-    char* body()
-    {
-      return data_ + header_length;
-    }
-
-    std::size_t body_length() const
-    {
-      return body_length_;
-    }
-
-    void body_length(std::size_t new_length)
-    {
-      body_length_ = new_length;
-      if (body_length_ > max_body_length)
-        body_length_ = max_body_length;
-    }
-
-    bool decode_header()
-    {
-      char header[header_length + 1] = "";
-      std::strncat(header, data_, header_length);
-      body_length_ = std::atoi(header);
-      if (body_length_ > max_body_length)
-      {
-        body_length_ = 0;
-        return false;
-      }
-      return true;
-    }
-
-    void encode_header()
-    {
-      char header[header_length + 1] = "";
-      std::sprintf(header, "%4d", static_cast<int>(body_length_));
-      std::memcpy(data_, header, header_length);
-    }
-
-    void setSender(std::string uuid)
-    {
-      sender_uuid = uuid;
-    }
-
-    void setReceiver(std::string uuid)
-    {
-      receiver_uuid = uuid;
-    }
-
-    std::string getReceiver()
-    {
-      return receiver_uuid;
-    }
-
-    std::string getSender()
-    {
-      return sender_uuid;
-    }
   
-  private:
-    std::string sender_uuid, receiver_uuid;
-    char data_[header_length + max_body_length];
-    std::size_t body_length_;
+
+  chat_message()
+    : body_length_(0)
+  {
+    //initialize here for good practice 
+    send_out = gen();
+    take_in = gen();
+    sender = boost::lexical_cast<std::string>(send_out);
+    receiver = boost::lexical_cast<std::string>(take_in);
+    
+  }
+
+  const char* data() const
+  {
+    return data_;
+  }
+
+  char* data()
+  {
+    return data_;
+  }
+
+  std::size_t length() const
+  {
+    return header_length + body_length_;
+  }
+
+  const char* body() const
+  {
+    return data_ + header_length;
+  }
+
+  char* body()
+  {
+    return data_ + header_length;
+  }
+
+  std::size_t body_length() const
+  {
+    return body_length_;
+  }
+
+  void body_length(std::size_t new_length)
+  {
+    body_length_ = new_length;
+    if (body_length_ > max_body_length)
+      body_length_ = max_body_length;
+  }
+
+  bool decode_header()
+  {
+    char header[header_length + 1] = "";
+    std::strncat(header, data_, header_length);
+    body_length_ = std::atoi(header);
+    if (body_length_ > max_body_length)
+    {
+      body_length_ = 0;
+      return false;
+    }
+    return true;
+  }
+
+  void encode_header()
+  {
+    char header[header_length + 1] = "";
+    std::sprintf(header, "%4d", static_cast<int>(body_length_));
+    std::memcpy(data_, header, header_length);
+  }
+
+private:
+  char data_[header_length + max_body_length];
+  std::size_t body_length_;
 };
 
 #endif // CHAT_MESSAGE_HPP

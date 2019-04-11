4cd6384b6c62317b418c0493b332ef7b718c31ba
David Nichols <david.nichols@mavs.uta.edu>
2019-04-11 17:11:22 -0500

    win_home.cpp: slight modifications
    same compilation instructions. again i'll integrate this by the end of the week

--- win_home.cpp
+++ win_home.cpp
@@ -28,37 +28,36 @@ int main()
 	tlc = trc = blc = brc = 42; //*
 	wborder(win_groups,left,right,top,bottom,tlc,trc,blc,brc);
 
-	mvwprintw(win_groups,1,8,"Group Chats");
-	mvwprintw(win_groups,1,25,"+");
-//	wmove(win_groups,2,xMax/5);
-	//refresh();
+	mvwprintw(win_groups,1,xMax/16,"Group Chats");
+	mvwprintw(win_groups,1,3*xMax/16,"+");
 	wrefresh(win_groups);
 
-	WINDOW * win_message = newwin(yMax/8,5*xMax/8,yMax/2+2*(yMax/5)-1,xMax/5);
+	WINDOW * win_message = newwin(yMax/8,5*xMax/8,7*yMax/8,xMax/5);
 	refresh();
 	box(win_message,0,0);
 	wrefresh(win_message);
 
 	WINDOW * win_users = newwin(yMax-2,11*xMax/64+1,1,33*xMax/40);
 	box(win_users,0,0);
-	mvwprintw(win_users,1,11,"Users");
+	mvwprintw(win_users,1,xMax/12,"Users");
 	wrefresh(win_users);
 
-	mvwprintw(win_groups,1,8,"Group Chats");
-	
+	WINDOW * win_groupname = newwin(3*yMax/32,5*xMax/8,1,xMax/5);
+	box(win_groupname,0,0);
+	wrefresh(win_groupname);
+
 	move(0,0);
 	hline('~',xMax);// line of ~
-	move(yMax/14,0);//move to draw top border
+	move(7*yMax/64,0);//move to draw top border
 	hline('-',xMax);// line of -
 	move(yMax-1,0);
 	hline('~',xMax);// line of ~ on bottom
 	
-	wmove(win_message,1,1);
-	
+	wmove(win_message,1,1); //move cursor to type input	
 
 	wrefresh(win_message);
 	refresh();
-	wscanw(win_message,"%s",str);
+//	wscanw(win_message,"%s",str);
 
 	getch();
 	endwin();
